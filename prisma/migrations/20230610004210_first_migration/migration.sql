5a242c5bf278986603a15345c15c30bd17f9b0e5
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-06-09 22:01:56 -0300

    create container for database postgresql with docker

---
+++ prisma/migrations/20230610004210_first_migration/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "org" (
+    "id" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+
+    CONSTRAINT "org_pkey" PRIMARY KEY ("id")
+);
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.oRG.create({
+  data: {
+    address: 'Rua Gabriel Ferreira',
+    phone: '86999654617',
+  },
+})
